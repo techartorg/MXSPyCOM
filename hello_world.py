28a6710b534a3ee0c18003dae23a32bd24bccd99
Jeff Hanna <jeff.b.hanna@gmail.com>
2019-09-13 05:52:52 -0500

    Updated HelloWorld.py to PySide2

--- hello_world.py
+++ hello_world.py
@@ -1,21 +1,11 @@
-from PySide import QtGui
+from PySide2.QtWidgets import QMessageBox
 
-
-class _GCProtector(object):
-	widgets = []
-
-app = QtGui.QApplication.instance()
-if not app:
-	app = QtGui.QApplication([])
-
-def main():
-	msg_box = QtGui.QMessageBox(MaxPlus.GetQMaxWindow())
-	_GCProtector.widgets.append(msg_box)
-
-	msg_box.setText("Hello World!")
-	msg_box.show()
+def main( ):
+	msg_box = QMessageBox( )	
+	msg_box.setText( 'Hello World!' )
+	msg_box.show( )
 
 
 
 if __name__ == '__main__':
-	main()
+	main( )
