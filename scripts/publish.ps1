cb2c72b97b4d04bfe06507cd63847b46d8a4c1ff
Jeff Hanna <jeff.b.hanna@gmail.com>
2022-01-09 16:04:06 -0600

    Expanded the publish script to optionally increment the version number.

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,3 +1,12 @@
+$inc_ver_num = Read-Host -Prompt "Increment the version number? (Y/N)?"
+if ($inc_ver_num -like "y") {
+    [xml]$axml= Get-Content "MXSPyCOM.csproj"
+    $ver_num_parts = $axml.Project.PropertyGroup.Version.Split(".")
+    $minor = [int]$ver_num_parts[-1] + 1
+    $axml.Project.PropertyGroup.Version = $ver_num_parts[0] + "." + $minor.ToString()
+    $axml.Save("MXSPyCOM.csproj")
+}
+
 dotnet publish "MXSPyCOM.csproj" -c Release
 
 mkdir "$env:TEMP\MXSPyCOM" -Force
@@ -10,3 +19,5 @@ Copy-Item ".\initialize_COM_server.ms" "$env:TEMP\MXSPyCOM"
 Compress-Archive -Path "$env:TEMP\MXSPyCOM\*" -DestinationPath "$env:USERPROFILE\DESKTOP\MXSPyCOM.zip" -Force
 
 Remove-Item "$env:TEMP\MXSPyCOM" -Recurse -Force
+
+Write-Output "Include the MXSPyCOM.zip file that was written to your desktop in the new binary release on GitHub."
