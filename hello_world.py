9c1ab1a635b91c89460221d151adef814f0cb8cf
Jeff Hanna <jeff.b.hanna@gmail.com>
2022-01-10 09:25:54 -0600

    Added type hints and reformatted to PEP8.

--- hello_world.py
+++ hello_world.py
@@ -1,26 +1,30 @@
-from pymxs import runtime
+"""
+A "Hello Word!" example as a Qt based messagebox. 
+This is used as a test to ensure MXSPyCOM is working correctly with 3ds Max.
+"""
+
+from pymxs import runtime # type: ignore
 
 try:
-    import MaxPlus
+    import MaxPlus # type: ignore
 except ImportError:
-    import shiboken2
+    import shiboken2 # type: ignore
     MaxPlus = None
 
-from PySide2.QtWidgets import QMainWindow, QMessageBox, QWidget
+from PySide2.QtWidgets import QMainWindow, QMessageBox, QWidget # type: ignore
 
 
-def main( ):
+def main() -> None:
     if MaxPlus:
-        main_window = MaxPlus.GetQMaxMainWindow( )
+        main_window = MaxPlus.GetQMaxMainWindow() # type: QMainWindow
     else:
-        main_window_widget = QWidget.find( int( runtime.windows.getMAXHWND( ) ) )
-        main_window = shiboken2.wrapInstance( shiboken2.getCppPointer( main_window_widget )[ 0 ], QMainWindow )
+        main_window_widget = QWidget.find(int(runtime.windows.getMAXHWND())) # type: QWidget
+        main_window = shiboken2.wrapInstance(shiboken2.getCppPointer(main_window_widget)[0], QMainWindow) # type: QMainWindow
 
-    msg_box = QMessageBox( main_window )
-    msg_box.setText( 'Hello World!' )
-    msg_box.show( )
-
+    msg_box = QMessageBox(main_window)
+    msg_box.setText('Hello World!')
+    msg_box.show()
 
 
 if __name__ == '__main__':
-    main( )
+    main()
